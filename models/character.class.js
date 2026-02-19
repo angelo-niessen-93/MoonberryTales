c3fbe07676be752717985dd65ff88e7e520c863b
angelo-niessen-93 <angeloniessen40@gmail.com>
2026-02-19 07:51:17 +0100

    added key to character added animation

--- models/character.class.js
+++ models/character.class.js
@@ -2,6 +2,7 @@ class Character extends MovableObject {
     height = 200;
     width = 100;
     y = 270;
+    speed = 4;
     IMAGES_WALKING = [
             '../img/Knight/Walk/walk1.png',
             '../img/Knight/Walk/walk2.png',
@@ -10,7 +11,7 @@ class Character extends MovableObject {
             '../img/Knight/Walk/walk5.png',
             '../img/Knight/Walk/walk6.png'
         ];
-        
+    world;    
 
 
     constructor() {
@@ -22,11 +23,28 @@ class Character extends MovableObject {
     }
 
     animate() {
+    
     setInterval(() => {
-        let i = this.currentImage % this.IMAGES_WALKING.length;
+        if (this.world.keyboard.RIGHT) {
+           this .x += this.speed;
+        }
+        if (this.world.keyboard.LEFT) {
+            this.x -= this.speed;
+        }
+
+    },1000 / 60);
+
+
+
+    setInterval(() => {
+        if (this.world.keyboard.RIGHT || this.world.keyboard.LEFT) {
+            
+        
+       let i = this.currentImage % this.IMAGES_WALKING.length;
        let path = this.IMAGES_WALKING[i];
        this.img = this.imageCache[path];
        this.currentImage++;
+       }
     }, 100);
  }
     jump(){
